f0e5889aac3d49a00b2ef923fd3201350b8ea116
Rakesh N <nraki522@gmail.com>
2026-05-02 11:14:17 +0530

    imported the models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,2 +1,6 @@
 from app.core.database import Base
 from .user import User
+from .resume import Resume
+from .jobdescription import JobDescription
+from .analysis import AnalysisReport
+from .coverletter import CoverLetterHistory
