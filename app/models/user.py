0006b6e50ab187d37d2be32107c78dd1ca9b3c0b
Rakesh N <nraki522@gmail.com>
2026-05-02 11:17:36 +0530

    updated the model fields

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column,Integer,String,TIMESTAMP,text
+from sqlalchemy import Column,Integer,String,Boolean,TIMESTAMP,text,Enum as SAEnum
 from enum import Enum as PyEnum
 from app.core.database import Base
 
@@ -12,5 +12,10 @@ class User(Base):
     id = Column(Integer,primary_key=True,nullable=False)
     full_name = Column(String,nullable=False)
     email = Column(String,unique=True,nullable=False)
-    password = Column(String,server_default='user',nullable=False)
-    role = Column(TIMESTAMP(timezone=True),nullable=False,server_default=text('now()'))
+    password = Column(String,nullable=False)
+    role = Column(SAEnum(UserRole,values_callable=lambda x:[e.value for e in x]),
+            default=UserRole.USER,
+            nullable=False
+    )
+    is_active = Column(Boolean,server_default='True',nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),nullable=False,server_default=text('now()'))
