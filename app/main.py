966758fd5ed6334102d3b07d3ef1a627e1d0661b
Rakesh N <nraki522@gmail.com>
2026-05-03 11:17:28 +0530

    imported the oauth file

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from . import models
 from app.db.database import engine
+from .routers import oauth, user
 
 models.Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
+app.include_router(user.router)
+app.include_router(oauth.router)
