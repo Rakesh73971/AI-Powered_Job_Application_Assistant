224fb7203f9f5d9955f07808fa28205a9cade703
Rakesh N <nraki522@gmail.com>
2026-05-07 13:45:54 +0530

    created the job desc schemas

--- app/schemas/jobdescription.py
+++ app/schemas/jobdescription.py
@@ -1,14 +1,20 @@
 from pydantic import BaseModel
 from datetime import datetime
+from typing import Optional
 
 class JodDescriptionCreate(BaseModel):
-    user_id: int
     company: str
     role_title: str
     jb_text: str
     created_at: datetime
 
+class JobDescriptionUpdate(BaseModel):
+    company: Optional[str] = None
+    role_title: Optional[str] = None
+    jb_text: Optional[str] = None
+
 class JobDescriptionResponse(JodDescriptionCreate):
+    user_id: int
     id: int
 
     class Config:
