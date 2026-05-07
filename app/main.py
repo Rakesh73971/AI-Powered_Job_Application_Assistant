71307d8585eb3f64eb9430a0675c2ad8ad6df282
Rakesh N <nraki522@gmail.com>
2026-05-07 13:49:48 +0530

    imported the routers

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from . import models
 from app.db.database import engine
-from .routers import oauth, user
+from .routers import oauth, user,resume,cover_letter,job_description
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -9,3 +9,6 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 app.include_router(user.router)
 app.include_router(oauth.router)
+app.include_router(resume.router)
+app.include_router(cover_letter.router)
+app.include_router(job_description.router)
