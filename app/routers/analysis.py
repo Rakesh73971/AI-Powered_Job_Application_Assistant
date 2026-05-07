2f552c51e26b808d532693e53a8a25df383ad078
Rakesh N <nraki522@gmail.com>
2026-05-07 15:22:07 +0530

    built the analysis apis

---
+++ app/routers/analysis.py
@@ -0,0 +1,36 @@
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.orm import Session
+from app.schemas.analysis import AnalysisReportResponse, AnalysisReportCreate, AnalysisReportUpdate
+from app.db.database import get_db
+from app.core.oauth2 import get_current_user, get_admin_user
+from app.services.analysis_service import create_analysis, get_analyses, get_analysis, update_analysis, delete_analysis
+from typing import List
+
+router = APIRouter(
+    prefix='/analyses',
+    tags=['Analyses']
+)
+
+@router.post('/', status_code=status.HTTP_201_CREATED, response_model=AnalysisReportResponse)
+def create_analysis_report(analysis: AnalysisReportCreate, db: Session = Depends(get_db), current_user = Depends(get_current_user)):
+    return create_analysis(db, analysis, current_user.id)
+
+@router.get('/', status_code=status.HTTP_200_OK, response_model=List[AnalysisReportResponse])
+def get_all_analyses(db: Session = Depends(get_db), current_user = Depends(get_admin_user)):
+    return get_analyses(db)
+
+@router.get('/{id}', status_code=status.HTTP_200_OK, response_model=AnalysisReportResponse)
+def get_analysis_report(id: int, db: Session = Depends(get_db), current_user = Depends(get_current_user)):
+    return get_analysis(db, id)
+
+@router.put('/{id}', status_code=status.HTTP_200_OK, response_model=AnalysisReportResponse)
+def put_analysis_report(id: int, analysis: AnalysisReportUpdate, db: Session = Depends(get_db), current_user = Depends(get_current_user)):
+    return update_analysis(db, id, analysis)
+
+@router.patch('/{id}', status_code=status.HTTP_200_OK, response_model=AnalysisReportResponse)
+def patch_analysis_report(id: int, analysis: AnalysisReportUpdate, db: Session = Depends(get_db), current_user = Depends(get_current_user)):
+    return update_analysis(db, id, analysis)
+
+@router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_analysis_report(id: int, db: Session = Depends(get_db), current_user = Depends(get_current_user)):
+    return delete_analysis(db, id)
