ad08d5429141a3dbfb8b97c13961feb3f902b8ed
Rakesh N <nraki522@gmail.com>
2026-05-07 13:48:15 +0530

    built the job desc services

---
+++ app/services/job_desc_service.py
@@ -0,0 +1,46 @@
+from sqlalchemy.orm import Session
+from app.models.jobdescription import JobDescription
+from fastapi import status,HTTPException
+
+def create_job_desc(db:Session,job):
+    db_job = JobDescription(
+        user_id = job.user_id,
+        company_name = job.company_name,
+        role_title = job.role_title,
+        jb_text = job.jb_text,
+    )
+    db.add(db_job)
+    db.commit()
+    db.refresh(db_job)
+    return db_job
+
+def get_job_descs(db:Session):
+    job_descriptions = db.query(JobDescription).all()
+    return job_descriptions
+
+def get_job_desc(db:Session,jd_id):
+    job_desc = db.query(JobDescription).filter(JobDescription.id == jd_id).first()
+    if not job_desc:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'job description with id {jd_id} not found')
+    return job_desc
+
+def update_job_desc(db:Session,jd_id,job_update):
+    job_desc = db.query(JobDescription).filter(JobDescription.id == jd_id).first()
+    if not job_desc:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'job description with id {jd_id} not found')
+    
+    update_data = job_update.model_dump(exclude_unset=True)
+    for key, value in update_data.items():
+        setattr(job_desc, key, value)
+        
+    db.commit()
+    db.refresh(job_desc)
+    return job_desc
+
+def delete_job_desc(db:Session,jd_id):
+    job_desc = db.query(JobDescription).filter(JobDescription.id == jd_id).first()
+    if not job_desc:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'job description with id {jd_id} not found')
+    db.delete(job_desc)
+    db.commit()
+    return job_desc
