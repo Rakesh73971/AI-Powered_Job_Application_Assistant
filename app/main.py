f102f332397644fc322cc825cd67a53921d0d646
Rakesh N <nraki522@gmail.com>
2026-05-07 15:23:52 +0530

    added the analysis api

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from . import models
 from app.db.database import engine
-from .routers import oauth, user,resume,cover_letter,job_description
+from .routers import oauth, user,resume,cover_letter,job_description, analysis
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -12,3 +12,4 @@ app.include_router(oauth.router)
 app.include_router(resume.router)
 app.include_router(cover_letter.router)
 app.include_router(job_description.router)
+app.include_router(analysis.router)
