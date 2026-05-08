87acba544b50c278e8f22e15834ac3b3d240d557
Rakesh N <nraki522@gmail.com>
2026-05-08 11:29:54 +0530

    added current user id

--- app/schemas/jobdescription.py
+++ app/schemas/jobdescription.py
@@ -3,19 +3,19 @@ from datetime import datetime
 from typing import Optional
 
 class JodDescriptionCreate(BaseModel):
-    company: str
+    company_name: str
     role_title: str
-    jb_text: str
-    created_at: datetime
+    jd_text: str
 
 class JobDescriptionUpdate(BaseModel):
     company: Optional[str] = None
     role_title: Optional[str] = None
-    jb_text: Optional[str] = None
+    jd_text: Optional[str] = None
 
 class JobDescriptionResponse(JodDescriptionCreate):
-    user_id: int
     id: int
+    user_id: int
+    created_at: datetime
 
     class Config:
         from_attributes=True
