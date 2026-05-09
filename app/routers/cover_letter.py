93701ab75f97f4f9dc79884c9bd7435de5ab70a9
Rakesh N <nraki522@gmail.com>
2026-05-09 12:04:53 +0530

    fixed the bugs

--- app/routers/cover_letter.py
+++ app/routers/cover_letter.py
@@ -16,21 +16,32 @@ router = APIRouter(
 def create_cover_letter(cover_letter: CoverLetterCreate, db: Session = Depends(get_db), current_user=Depends(get_current_user)):
     return create_cover_letter_service(db, cover_letter)
 
+
+
 @router.get('/', status_code=status.HTTP_200_OK, response_model=List[CoverLetterResponse])
 def get_cover_letters(db: Session = Depends(get_db), current_user=Depends(get_admin_user)):
     return get_cover_letters_service(db)
+
+
 
 @router.get('/{id}', status_code=status.HTTP_200_OK, response_model=CoverLetterResponse)
 def get_cover_letter(id: int, db: Session = Depends(get_db), current_user=Depends(get_current_user)):
     return get_cover_letter_service(db, id)
 
+
+
+
 @router.put('/{id}', status_code=status.HTTP_200_OK, response_model=CoverLetterResponse)
 def put_cover_letter(id: int, cover_letter: CoverLetterUpdate, db: Session = Depends(get_db), current_user=Depends(get_current_user)):
     return update_cover_letter_service(db, id, cover_letter)
 
+
+
 @router.patch('/{id}', status_code=status.HTTP_200_OK, response_model=CoverLetterResponse)
 def patch_cover_letter(id: int, cover_letter: CoverLetterUpdate, db: Session = Depends(get_db), current_user=Depends(get_current_user)):
     return update_cover_letter_service(db, id, cover_letter)
+
+
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def delete_cover_letter_endpoint(id: int, db: Session = Depends(get_db), current_user=Depends(get_current_user)):
