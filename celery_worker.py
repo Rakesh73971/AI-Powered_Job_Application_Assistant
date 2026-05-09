3e14b7c2a10aadb0b04051e9e2d0078214542584
Rakesh N <nraki522@gmail.com>
2026-05-09 12:27:41 +0530

    created celery

---
+++ celery_worker.py
@@ -0,0 +1,18 @@
+from celery import Celery
+from app.core.config import settings
+
+celery_app = Celery(
+    "ai_job_assistant",
+    broker=settings.redis_url,
+    backend=settings.redis_url,
+    include=["app.tasks.analysis_task"]
+)
+
+celery_app.conf.update(
+    task_serializer="json",
+    result_serializer="json",
+    accept_content=["json"],
+    timezone="UTC",
+    enable_utc=True,
+    task_track_started=True,
+)
