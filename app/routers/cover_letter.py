cee2617d09d62bdbf4fcdb1038c9b9f0c8197283
Rakesh N <nraki522@gmail.com>
2026-09-05 19:17:31 +0530

    changed the sync to async program

--- app/routers/cover_letter.py
+++ app/routers/cover_letter.py
@@ -1,11 +1,13 @@
-from fastapi import APIRouter,status,Depends
-from sqlalchemy.orm import Session
+from fastapi import APIRouter, status, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 from app.db.database import get_db
-from app.schemas.cover_letter import CoverLetterCreate,CoverLetterResponse, CoverLetterUpdate
-from app.services.cover_letter_service import create_cover_letter_service,get_cover_letters_service,get_cover_letter_service, update_cover_letter_service, delete_cover_letter
+from app.schemas.cover_letter import CoverLetterCreate, CoverLetterResponse, CoverLetterUpdate
+from app.services.cover_letter_service import (
+    create_cover_letter_service, get_cover_letters_service,
+    get_cover_letter_service, update_cover_letter_service, delete_cover_letter
+)
 from app.core.oauth2 import get_current_user, get_admin_user
 from typing import List
-
 
 router = APIRouter(
     prefix='/cover_letters',
@@ -13,36 +15,30 @@ router = APIRouter(
 )
 
 @router.post('/', status_code=status.HTTP_201_CREATED, response_model=CoverLetterResponse)
-def create_cover_letter(cover_letter: CoverLetterCreate, db: Session = Depends(get_db), current_user=Depends(get_current_user)):
-    return create_cover_letter_service(db, cover_letter)
-
+async def create_cover_letter(cover_letter: CoverLetterCreate, db: AsyncSession = Depends(get_db), current_user=Depends(get_current_user)):
+    return await create_cover_letter_service(db, cover_letter)
 
 
 @router.get('/', status_code=status.HTTP_200_OK, response_model=List[CoverLetterResponse])
-def get_cover_letters(db: Session = Depends(get_db), current_user=Depends(get_admin_user)):
-    return get_cover_letters_service(db)
-
+async def get_cover_letters(db: AsyncSession = Depends(get_db), current_user=Depends(get_admin_user)):
+    return await get_cover_letters_service(db)
 
 
 @router.get('/{id}', status_code=status.HTTP_200_OK, response_model=CoverLetterResponse)
-def get_cover_letter(id: int, db: Session = Depends(get_db), current_user=Depends(get_current_user)):
-    return get_cover_letter_service(db, id)
-
-
+async def get_cover_letter(id: int, db: AsyncSession = Depends(get_db), current_user=Depends(get_current_user)):
+    return await get_cover_letter_service(db, id)
 
 
 @router.put('/{id}', status_code=status.HTTP_200_OK, response_model=CoverLetterResponse)
-def put_cover_letter(id: int, cover_letter: CoverLetterUpdate, db: Session = Depends(get_db), current_user=Depends(get_current_user)):
-    return update_cover_letter_service(db, id, cover_letter)
-
+async def put_cover_letter(id: int, cover_letter: CoverLetterUpdate, db: AsyncSession = Depends(get_db), current_user=Depends(get_current_user)):
+    return await update_cover_letter_service(db, id, cover_letter)
 
 
 @router.patch('/{id}', status_code=status.HTTP_200_OK, response_model=CoverLetterResponse)
-def patch_cover_letter(id: int, cover_letter: CoverLetterUpdate, db: Session = Depends(get_db), current_user=Depends(get_current_user)):
-    return update_cover_letter_service(db, id, cover_letter)
-
+async def patch_cover_letter(id: int, cover_letter: CoverLetterUpdate, db: AsyncSession = Depends(get_db), current_user=Depends(get_current_user)):
+    return await update_cover_letter_service(db, id, cover_letter)
 
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def delete_cover_letter_endpoint(id: int, db: Session = Depends(get_db), current_user=Depends(get_current_user)):
-    return delete_cover_letter(db, id)
+async def delete_cover_letter_endpoint(id: int, db: AsyncSession = Depends(get_db), current_user=Depends(get_current_user)):
+    return await delete_cover_letter(db, id)
