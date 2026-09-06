bb345776d68024aea1fc19fcb7504925d2b79f89
Rakesh N <nraki522@gmail.com>
2026-09-06 22:58:24 +0530

    changed sync to async program

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter,status,Depends
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 from app.schemas.user import UserCreate,UserResponse, UserUpdate
 from app.db.database import get_db
 from app.services.user_service import create_user, get_users, get_user, update_user, delete_user
@@ -12,27 +12,27 @@ router = APIRouter(
 )
 
 @router.post('/',status_code=status.HTTP_201_CREATED,response_model=UserResponse)
-def create_db_user(user:UserCreate,db:Session=Depends(get_db)):
-    return create_user(db,user)
+async def create_db_user(user:UserCreate,db:AsyncSession=Depends(get_db)):
+    return await create_user(db,user)
 
 
 
 @router.get('/', status_code=status.HTTP_200_OK, response_model=List[UserResponse])
-def get_all_users(db: Session = Depends(get_db), current_user=Depends(get_admin_user)):
-    return get_users(db)
+async def get_all_users(db: AsyncSession = Depends(get_db), current_user=Depends(get_admin_user)):
+    return await get_users(db)
 
 @router.get('/{id}', status_code=status.HTTP_200_OK, response_model=UserResponse)
-def get_single_user(id: int, db: Session = Depends(get_db), current_user=Depends(get_current_user)):
-    return get_user(db, id)
+async def get_single_user(id: int, db: AsyncSession = Depends(get_db), current_user=Depends(get_current_user)):
+    return await get_user(db, id)
 
 @router.put('/{id}', status_code=status.HTTP_200_OK, response_model=UserResponse)
-def put_user(id: int, user: UserUpdate, db: Session = Depends(get_db), current_user=Depends(get_current_user)):
-    return update_user(db, id, user)
+async def put_user(id: int, user: UserUpdate, db: AsyncSession = Depends(get_db), current_user=Depends(get_current_user)):
+    return await update_user(db, id, user)
 
 @router.patch('/{id}', status_code=status.HTTP_200_OK, response_model=UserResponse)
-def patch_user(id: int, user: UserUpdate, db: Session = Depends(get_db), current_user=Depends(get_current_user)):
-    return update_user(db, id, user)
+async def patch_user(id: int, user: UserUpdate, db: AsyncSession = Depends(get_db), current_user=Depends(get_current_user)):
+    return await update_user(db, id, user)
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def delete_single_user(id: int, db: Session = Depends(get_db), current_user=Depends(get_current_user)):
-    return delete_user(db, id)
+async def delete_single_user(id: int, db: AsyncSession = Depends(get_db), current_user=Depends(get_current_user)):
+    return await delete_user(db, id)
